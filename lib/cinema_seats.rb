cb393adec833ac719e4fc72ea9ece26a54111a73
Colin Frankish <colin.frankish@gmail.com>
2014-08-16 17:49:03 +0100

    First commit

---
+++ lib/cinema_seats.rb
@@ -0,0 +1,53 @@
+class CinemaSeats
+   
+  def initialize 
+    @seatmap = ((1..50).to_a * 100).each_slice(50).to_a
+    @failed_bookings = []
+  end
+
+    attr_accessor :seatmap, :failed_bookings
+
+  def all_seats
+    @seatmap
+  end
+
+  def seat_request row, seat
+    @seatmap[row][seat]
+  end
+
+  def book_seat row, seat
+    bookit = @seatmap[row]
+    bookit.delete_at(seat)
+    bookit.insert(seat, "booked")
+    @seatmap.delete_at(row)
+    @seatmap.insert(row, bookit)
+  end
+
+  def access_data
+    File.open('test_data.txt', 'r').map do |d| 
+      d.scan(/\d+/).map { |s| s.to_i}
+    end
+  end
+
+  def booking_requests
+    access_data.map { |rq|  rq.values_at(1,2,4)}#values_at(1,2,4) #if valid_booking?
+  end
+
+  def make_booking(data=booking_requests)
+    row = data[0]
+    seats = data[1]..data[2]
+    seats.each { |seat| book_seat(row,seat)}
+  end
+
+  def same_row
+    access_data.each do |d|
+     d[1] != d[3] ? @failed_bookings << d : true
+    end
+  end
+
+  def less_than_six_seats
+    access_data.each do |d|
+      (d[4] - d[2] +1) > 5 ? @failed_bookings <<d : true 
+    end
+  end
+end
---
+++ spec/cinema_seats_spec.rb
@@ -0,0 +1,54 @@
+require 'cinema_seats'
+
+describe 'CinemaSeats'  do
+      let(:cinemaseats) {CinemaSeats.new}
+
+  it 'should be able to show an array of numbered seats' do
+    expect(cinemaseats.all_seats).to include(1..50)
+  end
+
+  it 'should have a total of 50 * 100 seats' do 
+    expect(cinemaseats.seatmap.flatten.count).to eq(5000)
+  end
+
+  it 'should know which seat is being requested ' do 
+    expect(cinemaseats.seat_request(0,2)).to eq(3)
+    expect(cinemaseats.seat_request(3,20)).to eq(21)
+  end
+
+  it 'should book a seat by row and seat index' do
+    cinemaseats.book_seat(0,2)
+    expect(cinemaseats.seatmap[0][2]).to eq("booked")
+    cinemaseats.book_seat(5,60)
+    expect(cinemaseats.seatmap[5][60]).to eq("booked")
+  end
+
+  it 'should extract all 5 data elements from the booking request file' do 
+    expect(cinemaseats.access_data).to include([0,89,13,89,13])
+  end
+  
+  it 'should extract booking requests from the request data file' do 
+    expect(cinemaseats.booking_requests).to include([16, 37, 37])
+    expect(cinemaseats.booking_requests).to include([89,13, 13])
+    expect(cinemaseats.booking_requests).to include([76,35, 37])
+    expect(cinemaseats.booking_requests[0]).to eq([89,13,13])
+    expect(cinemaseats.booking_requests[15]).to eq([91,26,27])
+  end
+
+  it 'should be able to make a booking of one to five seats' do 
+    cinemaseats.make_booking([89,13,13])
+    expect(cinemaseats.seatmap[89][13]).to eq("booked")
+    cinemaseats.make_booking([20,20,24])
+    expect(cinemaseats.seatmap[20][20]).to eq("booked")
+    expect(cinemaseats.seatmap[20][24]).to eq("booked")
+  end
+  it 'should not accept bookings with row requests on different rows' do 
+    cinemaseats.same_row
+    expect(cinemaseats.failed_bookings.length).to be(5)
+  end
+
+  it 'should reject booking requests for more than five seats' do 
+    cinemaseats.less_than_six_seats
+    expect(cinemaseats.failed_bookings.count).to be(4)
+  end
+end
